cf36886c488ce9328341fb5f47fb51567f421367
L Kevin Daniel <lrkevindaniel@gmail.com>
2025-12-23 23:52:43 +0530

    export data

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// Expose React as a global for builds that reference the `React` global
+// (prevents "React is not defined" runtime errors in some deployments)
+window.React = React;
 import App from "./App";
 import "./index.css";
 
